d32cc36c85512fbe8a40842bf47b077f55457884
blushell <micheal.jones89@gmail.com>
2024-11-30 09:08:39 -0500

    vercel

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -9,6 +9,9 @@ export default defineNuxtConfig({
 		'~/assets/css/tailwind.css',
 		'@mdi/font/css/materialdesignicons.min.css',
 	],
+	nitro: {
+		static: true,
+	},
 	app: {
 		head: {
 			title: 'Terminal Portfolio',
