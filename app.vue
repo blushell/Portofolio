2f4e58fd5bbbe3f1b4ad0aecdd88f31b6b247c7a
blushell <micheal.jones89@gmail.com>
2024-12-02 17:19:16 -0500

    update game over

--- app.vue
+++ app.vue
@@ -1,5 +1,12 @@
 <template>
 	<div>
 		<TerminalWindow />
+		<GameOver v-if="showGameOver" @close="closeGameOver" />
 	</div>
 </template>
+
+<script setup>
+import { useKonamiCode } from '~/composables/useKonamiCode';
+
+const { showGameOver, closeGameOver } = useKonamiCode();
+</script>
